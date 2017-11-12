4f297c1e73ca35ce66389879350296eeb034a914
Steven Halase <steven.halase@gmail.com>
2017-11-12 01:09:39 -0600

    Built out modals

--- www/js/main.js
+++ www/js/main.js
@@ -7,30 +7,35 @@ $(document).ready(() => {
     rotate: 360
   });
 
-  var CSStransforms = anime({
-    targets: '.hero-content svg',
-    opacity: 1,
-    marginTop: 0,
-    delay: 250
+  $('.nav-logo').hover(e => {
+    var CSStransforms = anime({
+      targets: '.nav-logo',
+      rotate: 360 * (Math.floor(Math.random() * 3) + 1)
+    });
+  })
+
+  var lineDrawing = anime({
+    targets: "#name-svg .lines path:last-of-type",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutQuart',
+    duration: 3000,
+    delay: 1500,
+    direction: 'alternate',
+    loop: true
   });
 
-  var CSStransforms = anime({
-    targets: '.hero-content p',
-    opacity: 1
-  });
-
-  var infiniteLoopAlternate = anime({
-    targets: '#name-svg text:last-of-type',
-    fill: [
-      { value: '#47c9e5', duration: 2000, delay: 1000 },
-      { value: '#FFF', duration: 2000, delay: 1000 }
-    ],
-    easing: 'easeInQuad',
-    loop: true,
-  });
+  tippy(document.querySelectorAll('.portfolio-item-tech'));
 
   $('.nav-menu-button').click(e => {
     $(e.target).toggleClass('open');
     $('.nav-menu-items').toggleClass('open');
+  })
+
+  $('.portfolio-item').click(e => {
+    $(e.target).find('.portfolio-item-modal').toggleClass('open');
+  })
+
+  $('.modal-close').click(e => {
+    $(e.target).parents('.portfolio-item-modal').toggleClass('open');
   })
 })
